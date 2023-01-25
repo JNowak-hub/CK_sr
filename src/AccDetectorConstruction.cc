996c6a3e8ab7ef018fd482fa44f966d4a2879ff1
jakub <nowakjakub095@gmail.com>
2023-01-25 15:19:58 +0100

    Phantom added with visualization

--- src/AccDetectorConstruction.cc
+++ src/AccDetectorConstruction.cc
@@ -82,6 +82,8 @@ G4VPhysicalVolume* AccDetectorConstruction::Construct()
  
   a = 16.00*g/mole;
    G4Element* elO = new G4Element(name="Oxygen",symbol="O", z=8., a);
+   G4Element* elH = new G4Element(name="Hydrogen", symbol="H", z=1., 1.00794*g/mole);
+   G4Element* elC = new G4Element(name="Carbon", symbol="C", z=1., 12.0107*g/mole);
 /* 
   a = 1.01*g/mole;
    G4Element* elH = new G4Element(name="Hydrogen",symbol="H", z=1., a);
@@ -98,13 +100,15 @@ G4VPhysicalVolume* AccDetectorConstruction::Construct()
   a = 58.7*g/mole;
    G4Element* elNi = new G4Element(name="Nickel",symbol="Ni", z=28., a);
    
-  a = 55.85*g/mole;
+
+  a = 55.85*g/mole;
    G4Element* elFe = new G4Element(name="Iron",symbol="Fe", z=26., a);
 
   a = 26.98*g/mole;
    G4Element* elAl = new G4Element(name="Aluminium",symbol="Al", z=13., a);
    
-  a = 40.078*g/mole;
+ 
+ a = 40.078*g/mole;
    G4Element* elCa = new G4Element(name="Calcium",symbol="Al", z=20., a);
 
   a = 137.372*g/mole;
@@ -137,9 +141,15 @@ G4VPhysicalVolume* AccDetectorConstruction::Construct()
   density = 1.290*mg/cm3;
   G4Material* Air = new G4Material(name="air", density, ncomponents=2);
   Air->AddElement(elN, fractionmass=0.7);
-  Air->AddElement(elO, fractionmass=0.3); 
+  Air->AddElement(elO, fractionmass=0.3);
+
+  G4Material* plexiGlass = new G4Material(name="plexiglass", 1.19*g/cm3, ncomponents=3);
+  plexiGlass->AddElement(elH, fractionmass=0.080538);
+  plexiGlass->AddElement(elC, fractionmass=0.599848);
+  plexiGlass->AddElement(elO, fractionmass=0.319614);
 /*
-//  density = 0.9*g/cm3;
+//
+  density = 0.9*g/cm3;
 //  G4Material* paraffin = new G4Material(name="paraffin", density, ncomponents=2);
 //  paraffin->AddElement(elC, natoms= 20);
 //  paraffin->AddElement(elH, natoms= 42); 
@@ -170,7 +180,8 @@ G4VPhysicalVolume* AccDetectorConstruction::Construct()
   concrete->AddElement(elAl,0.014245);
   concrete->AddElement(elFe,0.00285);
 
-// definition of BaSO4 concrete 
+// definition of BaSO4 concrete
+ 
 
   density = 3.5*g/cm3;
   G4Material* Baconcrete = new G4Material("Ba_concrete",density,8);
@@ -178,7 +189,8 @@ G4VPhysicalVolume* AccDetectorConstruction::Construct()
   Baconcrete->AddElement(elO,0.636105);
   Baconcrete->AddElement(elH,0.04986);
   Baconcrete->AddElement(elCa,0.02493);
-  Baconcrete->AddElement(elAl,0.0071225);
+  
+Baconcrete->AddElement(elAl,0.0071225);
   Baconcrete->AddElement(elFe,0.001425);
   Baconcrete->AddElement(elBa,0.0833);
   Baconcrete->AddElement(elS,0.0833);
@@ -283,313 +295,46 @@ natCu->AddIsotope(Cu5, abundance= 30.83*perCent);
   G4double World_z = 1.2*m;
   
   G4Box* solidWorld = new G4Box("World", // name of the volume
-                                World_x,World_y,World_z); // size of the volume
+                                World_x,
+                                World_y,
+                                World_z); // size of the volume
   G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, // name of the solid
-                                                Air, // material of the volume
-				                   "World"); // name of the volume
+                                                    Air, // material of the volume
+				                                            "World"); // name of the volume
 						    
   G4VPhysicalVolume* physWorld = new G4PVPlacement(0, // no rotation
                                                   G4ThreeVector(), // position at (0,0,0)
                                                  "World",  // name of the volume
                                                   logicWorld, // name of the logical volume
-					          0, // its mother volume
-					          false, // no boolean operation
-					          0); // copy number
+					                                        0, // its mother volume
+					                                        false, // no boolean operation
+					                                        0); // copy number
 
  
- // ------------------------------------------------------target
+ // ------------------------------------------------------phantom
+ G4double phantomX = 67.5/2*cm;
+ G4double phantomY = 64.5/2*cm;
+ G4double phantomZ = 56./2*cm;
  
-   G4double target_pRMin = 0.0*cm;   
-   G4double target_pRMax = (0.76/2)*cm;   
-   G4double target_pDz   = (0.4/2)*cm;   
-   G4double target_pSPhi = 0.*deg;   
-   G4double target_pDPhi = 360.*deg; 
-   G4Tubs* solidtarget = new G4Tubs("target",target_pRMin,target_pRMax,target_pDz,     
-                                     target_pSPhi,target_pDPhi);
-   G4LogicalVolume* logictarget = new G4LogicalVolume(solidtarget,Cu,"target");
-   G4double targetPos_x = 0.0*cm;
-   G4double targetPos_y = 0.0*cm;
-   G4double targetPos_z = 0.0*cm;
-    
-   G4VPhysicalVolume* phystarget
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(targetPos_x,targetPos_y,targetPos_z),
-				logictarget,"target",logicWorld,false,0);  
+ G4Box* phantom = new G4Box("Phantom", phantomX, phantomY, phantomY);
+ G4LogicalVolume* phantomVolume = new G4LogicalVolume(phantom, plexiGlass, "Phantom");
+ G4VPhysicalVolume* phantomPhysVolume = new G4PVPlacement(0, G4ThreeVector(),phantomVolume, "Phantom",logicWorld,false,0);
 
-      
-   G4VisAttributes* targetVisAtt = new G4VisAttributes(G4Colour(1,1,0));
-   targetVisAtt->SetForceSolid(true);
-   logictarget->SetVisAttributes(targetVisAtt);
-   
+ G4VisAttributes* phantomVisAtt = new G4VisAttributes(G4Colour(0,0,1));
+ phantomVisAtt->SetForceSolid(false);
+ phantomVolume->SetVisAttributes(phantomVisAtt);
 
-   
-   // ------------------------------------------------------collimator W1
- 
-   G4double colW1_pRMin = (1.86/2)*cm;   
-   G4double colW1_pRMax = (18.14/2)*cm;   
-   G4double colW1_pDz   = (5.28/2)*cm;   
-   G4double colW1_pSPhi = 0.*deg;   
-   G4double colW1_pDPhi = 360.*deg; 
-   G4Tubs* solidcolW1 = new G4Tubs("colW1",colW1_pRMin,colW1_pRMax,colW1_pDz,     
-                                     colW1_pSPhi,colW1_pDPhi);
-   G4LogicalVolume* logiccolW1 = new G4LogicalVolume(solidcolW1,W,"colW1");
-   G4double colW1Pos_x = 0.0*cm;
-   G4double colW1Pos_y = 0.0*cm;
-   G4double colW1Pos_z = (-5.28/2+0.2)*cm;
-    
-   G4VPhysicalVolume* physcolW1
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(colW1Pos_x,colW1Pos_y,colW1Pos_z),
-				logiccolW1,"colW1",logicWorld,false,0);  
+ G4double phantomWaterX = 66./2*cm;
+ G4double phantomWaterY = 63./2*cm;
+ G4double phantomWaterZ = 41./2*cm;
 
-      
-   G4VisAttributes* colW1VisAtt = new G4VisAttributes(G4Colour(0,0,1));
-   colW1VisAtt->SetForceSolid(true);
-   logiccolW1->SetVisAttributes(colW1VisAtt);
+ G4Box* phantomWater = new G4Box("Phantom", phantomX, phantomY, phantomY);
+ G4LogicalVolume* phantomWaterVolume = new G4LogicalVolume(phantom, plexiGlass, "Phantom");
+ G4VPhysicalVolume* phantomWaterPhysVolume = new G4PVPlacement(0, G4ThreeVector(0., 0., -7.5*cm) ,phantomWaterVolume, "Phantom", phantomVolume, false, 0);
 
- 
-   // ------------------------------------------------------collimator W2a
- 
-   G4double colW2a_pRMin = (0.57/2)*cm;   
-   G4double colW2a_pRMax = (18.14/2)*cm;   
-   G4double colW2a_pDz   = (1.43/2)*cm;   
-   G4double colW2a_pSPhi = 0.*deg;   
-   G4double colW2a_pDPhi = 360.*deg; 
-   G4Tubs* solidcolW2a = new G4Tubs("colW2a",colW2a_pRMin,colW2a_pRMax,colW2a_pDz,     
-                                     colW2a_pSPhi,colW2a_pDPhi);
-   G4LogicalVolume* logiccolW2a = new G4LogicalVolume(solidcolW2a,W,"colW2a");
-   G4double colW2aPos_x = 0.0*cm;
-   G4double colW2aPos_y = 0.0*cm;
-   G4double colW2aPos_z = (1.43/2+0.2)*cm;
-    
-   G4VPhysicalVolume* physcolW2a
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(colW2aPos_x,colW2aPos_y,colW2aPos_z),
-				logiccolW2a,"colW2a",logicWorld,false,0);  
-
-      
-   G4VisAttributes* colW2aVisAtt = new G4VisAttributes(G4Colour(1,0,0));
-   colW2aVisAtt->SetForceSolid(true);
-   logiccolW2a->SetVisAttributes(colW2aVisAtt);
-
-
-   // ------------------------------------------------------collimator W2b
- 
-  G4double phiStart = 0;
-  G4double phiTotal = 360*deg;
-  G4int numZPlanes = 2;
-  const G4double zPlane2b[] = { (1.43+0.2)*cm,(1.43+0.2+6.7)*cm};
-  const G4double rInner2b[] = { (0.57/2)*cm, (6.43/2)*cm};
-  const G4double rOuter2b[] = { (18.14/2)*cm, (18.14/2)*cm};
-
-  G4Polycone* solidpcolW2b = new G4Polycone("W2b",phiStart,phiTotal,numZPlanes,
-                                                           zPlane2b,rInner2b,rOuter2b);
-							   
-   G4LogicalVolume* logiccolW2b = new G4LogicalVolume(solidpcolW2b,W,"W2b");				     
-   
-   G4double pcolW2bPos_x = 0.0*cm;
-   G4double pcolW2bPos_y = 0.0*cm;
-   G4double pcolW2bPos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW2b
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW2bPos_x,pcolW2bPos_y,pcolW2bPos_z),
-				logiccolW2b,"W2b",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW2bVisAtt = new G4VisAttributes(G4Colour(0,1,0));
-   colW2bVisAtt->SetForceSolid(true);
-   logiccolW2b->SetVisAttributes(colW2bVisAtt);
-   
-
-   // ------------------------------------------------------collimator W3
- 
- // G4double phiStart = 0;
-//  G4double phiTotal = 360*deg;
-//  G4int numZPlanes = 2;
-  const G4double zPlaneW3[] = { (1.43+0.2+6.7)*cm,(1.43+0.2+6.7+3.57)*cm};
-  const G4double rInnerW3[] = { (1.29/2)*cm, (1.86/2)*cm};
-  const G4double rOuterW3[] = { (15.57/2)*cm, (11.43/2)*cm};
-
-  G4Polycone* solidpcolW3 = new G4Polycone("W3",phiStart,phiTotal,numZPlanes,
-                                                           zPlaneW3,rInnerW3,rOuterW3);
-							   
-   G4LogicalVolume* logiccolW3 = new G4LogicalVolume(solidpcolW3,W,"W3");				     
-   
-   G4double pcolW3Pos_x = 0.0*cm;
-   G4double pcolW3Pos_y = 0.0*cm;
-   G4double pcolW3Pos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW3
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW3Pos_x,pcolW3Pos_y,pcolW3Pos_z),
-				logiccolW3,"W3",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW3VisAtt = new G4VisAttributes(G4Colour(1,0,1));
-   colW3VisAtt->SetForceSolid(true);
-   logiccolW3->SetVisAttributes(colW3VisAtt);
-
- // ------------------------------------------------------collimator W4a
- 
- // G4double phiStart = 0;
- // G4double phiTotal = 360*deg;
- // G4int numZPlanes = 2;
-  const G4double zPlaneW4a[] = { (1.43+0.2+6.7+3.57+6.0)*cm, (1.43+0.2+6.7+3.57+6.0+1.0)*cm};
-  const G4double rInnerW4a[] = { (1.29/2)*cm, (1.86/2)*cm};
-  const G4double rOuterW4a[] = { (4.57/2)*cm, (4.57/2)*cm};
-
-  G4Polycone* solidpcolW4a = new G4Polycone("W4a",phiStart,phiTotal,numZPlanes,
-                                                           zPlaneW4a,rInnerW4a,rOuterW4a);
-							   
-   G4LogicalVolume* logiccolW4a = new G4LogicalVolume(solidpcolW4a,W,"W4a");				     
-   
-   G4double pcolW4aPos_x = 0.0*cm;
-   G4double pcolW4aPos_y = 0.0*cm;
-   G4double pcolW4aPos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW4a
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW4aPos_x,pcolW4aPos_y,pcolW4aPos_z),
-				logiccolW4a,"W4a",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW4aVisAtt = new G4VisAttributes(G4Colour(1,1,0));
-   colW4aVisAtt->SetForceSolid(true);
-   logiccolW4a->SetVisAttributes(colW4aVisAtt);
-
-// ------------------------------------------------------collimator W4b
- 
- // G4double phiStart = 0;
- // G4double phiTotal = 360*deg;
- // G4int numZPlanes = 2;
-  const G4double zPlaneW4b[] = { (1.43+0.2+6.7+3.57+6.0+1.0)*cm, (1.43+0.2+6.7+3.57+6.0+1.0+2.29)*cm};
-  const G4double rInnerW4b[] = { (1.71/2)*cm, (1.86/2)*cm};
-  const G4double rOuterW4b[] = { (14.7/2)*cm, (14.7/2)*cm};
-
-  G4Polycone* solidpcolW4b = new G4Polycone("W4b",phiStart,phiTotal,numZPlanes,
-                                                           zPlaneW4b,rInnerW4b,rOuterW4b);
-							   
-   G4LogicalVolume* logiccolW4b = new G4LogicalVolume(solidpcolW4b,W,"W4b");				     
-   
-   G4double pcolW4bPos_x = 0.0*cm;
-   G4double pcolW4bPos_y = 0.0*cm;
-   G4double pcolW4bPos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW4b
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW4bPos_x,pcolW4bPos_y,pcolW4bPos_z),
-				logiccolW4b,"W4b",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW4bVisAtt = new G4VisAttributes(G4Colour(1,1,0));
-   colW4bVisAtt->SetForceSolid(true);
-   logiccolW4b->SetVisAttributes(colW4bVisAtt);
-
-// ------------------------------------------------------collimator W4c
- 
- // G4double phiStart = 0;
- // G4double phiTotal = 360*deg;
- // G4int numZPlanes = 2;
-  const G4double zPlaneW4c[] = { (1.43+0.2+6.7+3.57+6.0+1.0+2.29)*cm, (1.43+0.2+6.7+3.57+6.0+1.0+2.29+0.5)*cm};
-  const G4double rInnerW4c[] = { (1.86/2)*cm, (1.93/2)*cm};
-  const G4double rOuterW4c[] = { (8.43/2)*cm, (8.42/2)*cm};
-
-  G4Polycone* solidpcolW4c = new G4Polycone("W4c",phiStart,phiTotal,numZPlanes,
-                                                           zPlaneW4c,rInnerW4c,rOuterW4c);
-							   
-   G4LogicalVolume* logiccolW4c = new G4LogicalVolume(solidpcolW4c,W,"W4c");				     
-   
-   G4double pcolW4cPos_x = 0.0*cm;
-   G4double pcolW4cPos_y = 0.0*cm;
-   G4double pcolW4cPos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW4c
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW4cPos_x,pcolW4cPos_y,pcolW4cPos_z),
-				logiccolW4c,"W4c",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW4cVisAtt = new G4VisAttributes(G4Colour(1,1,0));
-   colW4cVisAtt->SetForceSolid(true);
-   logiccolW4c->SetVisAttributes(colW4cVisAtt);
-
-// ------------------------------------------------------collimator W4d
- 
- // G4double phiStart = 0;
- // G4double phiTotal = 360*deg;
- // G4int numZPlanes = 2;
-  const G4double zPlaneW4d[] = { (1.43+0.2+6.7+3.57+6.0+1.0+2.29+0.5)*cm, (1.43+0.2+6.7+3.57+6.0+1.0+2.29+0.5+4.71)*cm};
-  const G4double rInnerW4d[] = { (1.93/2)*cm, (2.29/2)*cm};
-  const G4double rOuterW4d[] = { (6.43/2)*cm, (6.43/2)*cm};
-
-  G4Polycone* solidpcolW4d = new G4Polycone("W4d",phiStart,phiTotal,numZPlanes,
-                                                           zPlaneW4d,rInnerW4d,rOuterW4d);
-							   
-   G4LogicalVolume* logiccolW4d = new G4LogicalVolume(solidpcolW4d,W,"W4d");				     
-   
-   G4double pcolW4dPos_x = 0.0*cm;
-   G4double pcolW4dPos_y = 0.0*cm;
-   G4double pcolW4dPos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW4d
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW4dPos_x,pcolW4dPos_y,pcolW4dPos_z),
-				logiccolW4d,"W4d",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW4dVisAtt = new G4VisAttributes(G4Colour(1,1,0));
-   colW4dVisAtt->SetForceSolid(true);
-   logiccolW4d->SetVisAttributes(colW4dVisAtt);
-
- // ------------------------------------------------------collimator W5
- 
- // G4double phiStart = 0;
- // G4double phiTotal = 360*deg;
-//  G4int numZPlanes = 2;
-  const G4double zPlane[] = { (1.43+0.2+6.7+3.57+6.0+1.0+2.29+0.5+4.71+6.0)*cm,(1.43+0.2+6.7+3.57+6.0+1.0+2.29+0.5+4.71+6.0+9.22)*cm};
-  const G4double rInner[] = { (2.45/2)*cm, (3.02/2)*cm};
-  const G4double rOuter[] = { (4.0/2)*cm, (5.7/2)*cm};
-
-  G4Polycone* solidpcolW5 = new G4Polycone("W5",phiStart,phiTotal,numZPlanes,
-                                                           zPlane,rInner,rOuter);
-							   
-   G4LogicalVolume* logiccolW5 = new G4LogicalVolume(solidpcolW5,W,"W5");				     
-   
-   G4double pcolW5Pos_x = 0.0*cm;
-   G4double pcolW5Pos_y = 0.0*cm;
-   G4double pcolW5Pos_z = 0.0*cm;
-   G4VPhysicalVolume* physpcolW5
-                  = new G4PVPlacement(0,
-                                G4ThreeVector(pcolW5Pos_x,pcolW5Pos_y,pcolW5Pos_z),
-				logiccolW5,"W5",logicWorld,false,0);
-
-      
-   G4VisAttributes* colW5VisAtt = new G4VisAttributes(G4Colour(0,1,1));
-   colW5VisAtt->SetForceSolid(true);
-   logiccolW5->SetVisAttributes(colW5VisAtt);
-
-  //   ----------------------------------------------detectors
-
- // Detector for spectrum measurements
- 
-   G4double Det_pRMin = 0.0*cm;   
-   G4double Det_pRMax = 3.0*cm;   
-   G4double Det_pDz   = .05*mm;   
-   G4double Det_pSPhi = 0.*deg;    
-   G4double Det_pDPhi = 360.*deg;   
-   G4Tubs* solidDet = new G4Tubs("Detector",Det_pRMin,Det_pRMax,Det_pDz,
-                                       Det_pSPhi,Det_pDPhi);
-  
-  G4LogicalVolume* 
-                     logicDet = new G4LogicalVolume(solidDet,Air,"Detector");
-  G4double DetPos_x = 0.0*cm;
-  G4double DetPos_y = 0.0*cm;
-  G4double DetPos_z = 59.9*cm;
-  G4VPhysicalVolume* physDet
-    = new G4PVPlacement(0,
-                        G4ThreeVector(DetPos_x,DetPos_y,DetPos_z),
-                        logicDet,"Detector",logicWorld,false,0); 
-
-
-   G4VisAttributes* DetVisAtt = new G4VisAttributes(G4Colour(1,1,1));
-   DetVisAtt->SetForceSolid(true);
-   logicDet->SetVisAttributes(DetVisAtt);
-
+ G4VisAttributes* phantomWaterVisAtt = new G4VisAttributes(G4Colour(0,0,2));
+ phantomWaterVisAtt->SetForceSolid(true);
+ phantomWaterVolume->SetVisAttributes(phantomVisAtt);
  return physWorld;
  }
 
