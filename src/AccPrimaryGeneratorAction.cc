eea82c285978ffec9a342dc13a082381801c0b63
jakub <nowakjakub095@gmail.com>
2023-02-27 14:59:54 +0100

    Still geantino

--- src/AccPrimaryGeneratorAction.cc
+++ src/AccPrimaryGeneratorAction.cc
@@ -1,40 +1,50 @@
-﻿// $Id: AccPrimaryGeneratorAction.cc, 2005
-// -------------------------------------------------------------------
-//    by Adam Konefal, based on GEANT 4
-// -------------------------------------------------------------------
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// Authors: Susanna Guatelli and Francesco Romano
+// susanna@uow.edu.au, francesco.romano@ct.infn.it
+//
 
 #include "AccPrimaryGeneratorAction.hh"
 #include "G4Event.hh"
-#include "G4ParticleGun.hh"
-#include "G4ParticleTable.hh"
-#include "G4ParticleDefinition.hh"
-#include "G4UImanager.hh"
-#include "globals.hh"
-#include "Randomize.hh"
-#include <math.h>
-#include <fstream>
-#include <iostream>
-#include "G4SystemOfUnits.hh"
 #include "G4GeneralParticleSource.hh"
-
-using namespace std;
 
 AccPrimaryGeneratorAction::AccPrimaryGeneratorAction()
 {
-  particleGun = new G4GeneralParticleSource();
+ gun = new G4GeneralParticleSource();
 }
 
 AccPrimaryGeneratorAction::~AccPrimaryGeneratorAction()
 {
-  delete particleGun;
-}
+ delete gun;
+}	
 
 void AccPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
-  particleGun -> GeneratePrimaryVertex(anEvent);
+ gun -> GeneratePrimaryVertex(anEvent);
 }
-
-
 
 
 
